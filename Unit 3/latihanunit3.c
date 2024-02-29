8b588b5bade69207d80b6dcc498ef7a86ddddf12
estehangat <96716034+estehangat@users.noreply.github.com>
2024-02-29 14:49:54 +0700

    Add files via upload

---
+++ Unit 3/latihanunit3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    int jumlah = 0;
+
+    printf("Masukkan jumlah array : ");
+    scanf("%d", &n);
+    int array[n];
+
+    printf("Masukkan integer :\n");
+    for(int i = 0; i < n; i++){
+        scanf("%d", &array[i]);
+    }
+
+    for(int j = 0; j < n; j++){
+        jumlah = jumlah + array[j];
+    }
+
+    printf("Hasil penjumlahan seluruh elemen pada array adalah %d", jumlah);
+
+    return 0;
+}
